c0321b36e91bd976ab7634592e46bef0e5c2af9e
Replit user <>
2023-03-24 17:28:27 +0000

    new try

--- list.c
+++ list.c
@@ -82,9 +82,18 @@ void * prevList(List * list)
 void pushFront(List * list, void * data) 
 {
   Node * newNode = createNode(data);
-  newNode->next = list->head;
-  list->head->prev = newNode;
-  list->head = newNode;
+  if (list->head == NULL) 
+  {
+    list->head = newNode;
+    list->tail = newNode;
+    list->current = newNode;
+  } 
+  else 
+  {
+    newNode->next = list->head;
+    list->head->prev = newNode;
+    list->head = newNode;
+  }
 }
 
 void pushBack(List * list, void * data) 
